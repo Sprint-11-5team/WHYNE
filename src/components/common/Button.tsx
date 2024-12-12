45ef6671a4cb9fa234fe6ca9aa6534996ec129a0
lemon1335 <kimjinhui114@gmail.com>
2024-12-12 17:54:49 +0900

    [chore] 파일 초기 설정

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,17 @@
+import React, { ButtonHTMLAttributes, ReactNode } from 'react';
+
+interface ButtonProp extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+function Button({ children, ...rest }: ButtonProp) {
+  return (
+    <>
+      <button {...rest}>
+        <div>{children}</div>
+      </button>
+    </>
+  );
+}
+
+export default Button;
