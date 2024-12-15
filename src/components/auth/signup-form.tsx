b99a886c93671ed7b8cf4d15b0180b9578c63727
ksj <naver.daum0711@gmail.com>
2024-12-15 17:13:50 +0900

    Feat: 회원가입 틀 구현(css 전)

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -1,7 +1,27 @@
-export default function Form() {
+import InputItem from "./input-item";
+
+export default function SignUpForm() {
   return (
     <form>
-      <input></input>
+      <InputItem
+        label="이메일"
+        id="email"
+        type="email"
+        placeholder="whyne@email.com"
+      />
+      <InputItem label="닉네임" id="nickname" type="text" placeholder="whyne" />
+      <InputItem
+        label="비밀번호"
+        id="pw"
+        type="password"
+        placeholder="영문, 숫자 포함 8자 이상"
+      />
+      <InputItem
+        label="비밀번호 확인"
+        id="pw-check"
+        type="password"
+        placeholder="비밀번호 확인"
+      />
     </form>
   );
 }
