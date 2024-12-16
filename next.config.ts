7ae3dc0f9be447de998ea7b5f7c6aac083125954
lemon1335 <kimjinhui114@gmail.com>
2024-12-16 10:45:54 +0900

    [etc] 필요한 파일들 받기

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,
+    domains: ["http://www.w3.org/2000/***"],
+  },
 };
 
 export default nextConfig;
