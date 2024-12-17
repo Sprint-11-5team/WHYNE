9a300197b3c76fc2f283d4feaa6de6058bea75a6
part3-5 <part3_team5@gmail.com>
2024-12-17 16:08:48 +0900

    라벨 태그로 변경, css 수정

--- src/app/input-test/page.tsx
+++ src/app/input-test/page.tsx
@@ -6,19 +6,19 @@ export default function Home() {
         <div className="space-y-4">
           {/* 기본 사용 */}
           <div>
-            <h2>기본 입력필드:</h2>
+            <label className="block">기본 입력필드:</label>
             <Input placeholder="기본 입력필드" />
           </div>
   
           {/* 에러 상태 */}
           <div>
-            <h2>에러 상태:</h2>
+            <label className="block">에러 상태:</label>
             <Input placeholder="에러 상태" isErrored={true} />
           </div>
   
           {/* 너비 제한 */}
           <div className="w-[20.4375rem] tablet:w-[25.75rem]">
-            <h2>너비 제한된 입력필드:</h2>
+            <label className="block">너비 제한된 입력필드:</label>
             <Input placeholder="너비 제한" />
           </div>
         </div>
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -23,7 +23,8 @@ export default function Input({
   tablet:rounded-[1.6rem] 
   tablet:text-[1.4rem]
   desktop:text-[1.6rem]
-  ${isErrored ? "border-red" : "border-[0.1rem] border-gray-300"}  // border 속성을 하나로 합침
+  ${isErrored ? "border-[0.1rem] border-red" : "border-[0.1rem] border-gray-300"}
+ // border 속성을 하나로 합침
   ${className ? ` ${className}` : ""}
   `;
   
