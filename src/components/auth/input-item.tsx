3563275b4e9d979d6f91d8290f2078c17176c17f
ksj <naver.daum0711@gmail.com>
2024-12-18 19:11:13 +0900

    Feat: 유효성 검사(비밀번호 확인 제외 완성)

--- src/components/auth/input-item.tsx
+++ src/components/auth/input-item.tsx
@@ -1,28 +1,89 @@
 "use client"; // useState 훅 사용 위해 클라이언트 컴포넌트로 명시(멘토님이 말씀주신 서버 클라이언트 제약사항?)
 
 import { useState } from "react";
+// import { validationRules } from "./rules";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  // rules?: typeof validationRules.rules;
   minLengthRule?: number;
+  maxLengthRule?: number;
   validationRule?: string;
-  errorMessage?: string;
+  emptyErrorMessage?: string;
+  minLengthErrorMessage?: string;
+  maxLengthErrorMessage?: string;
+  validationMessage?: string;
 }
 
 export default function InputItem({
   label,
   id,
+  // rules,
   minLengthRule,
+  maxLengthRule,
   validationRule,
-  errorMessage = "입력값이 유효하지 않습니다.",
+  emptyErrorMessage = "필수 입력입니다.",
+  minLengthErrorMessage = "최소 글자 수 기준보다 적습니다.",
+  maxLengthErrorMessage = "최대 글자 수 기준보다 많습니다.",
+  validationMessage = "입력값이 유효하지 않습니다.",
+  required,
   ...props
 }: InputProps) {
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // const handleEmptyInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const value = e.target.value;
+  //   if (required && !value) {
+  //     setError(true);
+  //     setErrorMessage(rules?.messages.empty || "필수 입력입니다.");
+  //   } else {
+  //     setError(false);
+  //     setErrorMessage("");
+  //   }
+  // };
+
+  // const handleValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const value = e.target.value;
+  //   if (rules?.minLength && value.length < rules.minLength) {
+  //     setError(true);
+  //     setErrorMessage(
+  //       rules.messages.minLength || "최소 글자 수 기준보다 짧습니다.",
+  //     );
+  //     return;
+  //   }
+  //   if (rules?.regex && !new RegExp(rules.regex).test(value)) {
+  //     setError(true);
+  //     setErrorMessage(rules.messages.invalid || "유효하지 않은 값입니다.");
+  //     return;
+  //   }
+  //   if (rules?.maxLength && value.length < rules.maxLength) {
+  //     setError(true);
+  //     setErrorMessage(
+  //       rules.messages.maxLength || "최대 글자 수 기준보다 깁니다.",
+  //     );
+  //     return;
+  //   }
+  //   setError(false);
+  //   setErrorMessage("");
+  // };
+
+  const handleEmptyInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (required && !value) {
+      setError(true);
+      setErrorMessage(emptyErrorMessage);
+      return;
+    }
+  };
 
   const handleValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
+
     if (minLengthRule && value.length < minLengthRule) {
       setError(true);
+      setErrorMessage(minLengthErrorMessage);
+      return;
     }
 
     if (validationRule) {
@@ -30,9 +91,24 @@ export default function InputItem({
       const regex = new RegExp(validationRule);
       if (!regex.test(value)) {
         setError(true);
-      } else {
-        setError(false);
+        setErrorMessage(validationMessage);
+        return;
       }
+    }
+
+    if (maxLengthRule && value.length > maxLengthRule) {
+      setError(true);
+      setErrorMessage(maxLengthErrorMessage);
+      return;
+    }
+
+    setError(false);
+    setErrorMessage("");
+  };
+
+  const handlePreventSpace = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === " ") {
+      e.preventDefault();
     }
   };
 
@@ -45,6 +121,8 @@ export default function InputItem({
         id={id}
         className={`placeholder-gray-500 text-[1.6rem] border border-gray-300 rounded-[1.6rem] w-[40rem] h-[4.8rem] pl-[2rem]
         ${error ? "border-red" : "border-gray-300"}`}
+        onKeyDown={handlePreventSpace}
+        onBlur={handleEmptyInput}
         onChange={handleValidation}
         {...props}
       />
---
+++ src/types/rules.d.ts
@@ -0,0 +1,15 @@
+// declare module "./rules" {
+//   export const validationRules: {
+//     rules?: {
+//       regex?: string;
+//       minLength?: number;
+//       maxLength?: number;
+//       messages: {
+//         empty?: string;
+//         minLength?: string;
+//         maxLength?: string;
+//         invalid?: string;
+//       };
+//     };
+//   };
+// }
