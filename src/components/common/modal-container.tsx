850b83e6b53808b624af5aaa12d1a195ccb425e3
lemon1335 <kimjinhui114@gmail.com>
2024-12-25 17:24:04 +0900

    [style] modal-container 공통 css 수정

--- src/components/common/modal-container.tsx
+++ src/components/common/modal-container.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useState } from "react";
 
 type ModalProps = {
@@ -48,27 +49,25 @@ function Modal({ isOpen, onClose, children, className = "" }: ModalProps) {
         className="fixed inset-0 bg-black opacity-50 transition-opacity duration-300"
         onClick={onClose}
       />
+
       <div
         className={`
+          flex flex-col h-auto
           relative z-20
           bg-white 
           w-full
-          tablet:max-w-[46rem]
-          h-fix
-          max-h-screen
-          
-          rounded-t-[2rem] tablet:rounded-[2rem]
-          flex flex-col
-          overflow-hidden
-          py-[0.4rem] mobile:py-[0.6rem] tablet:py-[0.8rem] // 이렇게 화면 크기별 패딩 추가
-
+          mobile:h-auto
+          overflow-auto
+          m-auto
           ${getAnimationClass()}
           ${className}
         `}
       >
-        <div className="flex flex-col overflow-auto">{children}</div>
+        {/* <div className="flex flex-col h-full overflow-auto my-0 mx-auto"> */}
+        {children}
       </div>
     </div>
+    // </div>
   );
 }
 
