044d09177cd2a1e9ce9263ed7c7ec5ce118c79a9
minjin <geniusmj02@gmail.com>
2024-12-30 22:50:07 +0900

    내 프로필 상세 페이지 qa 반영 1차

--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -3,8 +3,8 @@ import ProfileTab from "@/components/myprofile/profile-tab";
 
 export default function MyProfilePage() {
   return (
-    <div className="desktop:mt-[3.7rem] tablet:mt-[2rem] mobile:mt-[2rem] justify-center flex">
-      <div className="flex desktop:flex-row tablet:flex-col mobile:flex-col">
+    <div className="desktop:mt-[3.7rem] tablet:mt-[2rem] mobile:mt-[2rem] justify-center flex w-full">
+      <div className="flex justify-center desktop:flex-row tablet:flex-col mobile:flex-col w-full">
         <div className="desktop:mr-[6rem] desktop:ml-0 tablet:mx-[2rem] mobile:mx-[1.8rem]">
           <Profile />
         </div>
