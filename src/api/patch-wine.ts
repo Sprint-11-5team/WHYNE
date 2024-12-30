3a0742db49da5cfb7cbd386e74484f360821de53
part3-5 <part3_team5@gmail.com>
2024-12-30 16:15:47 +0900

    와인 수정모달 API

---
+++ src/api/patch-wine.ts
@@ -0,0 +1,24 @@
+import instance from "./api";
+
+interface WineData {
+  name: string;
+  region: string;
+  image: string;
+  price: number;
+  type: string;
+}
+
+
+export default async function updateWine(
+  id: number,
+  wineData: WineData,
+): Promise<WineData> {
+  try {
+    const res = await instance.patch<WineData>(`wines/${id}`, wineData);
+    console.log("와인 수정 성공:", res.data);
+    return res.data; // 수정된 와인 데이터 반환
+  } catch (error) {
+    console.error("와인 수정 실패:", error);
+    throw error; // 에러를 호출자에게 전달
+  }
+}
